e718a5fdddb32e040febad0cc4a247e655488684
Abdullah Rashid <229150035+abdullahrashid359@users.noreply.github.com>
2026-09-01 20:28:36 +0500

    style: polish Spill message board

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const indexRouter = require('./routes/indexRouter');
 
 const app = express();
 
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
